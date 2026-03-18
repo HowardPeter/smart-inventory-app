cc425101a0b703cdce33ebbbed0c0460423b8297
HowardPeter <phuocnt1611@gmail.com>
2026-03-18 18:28:48 +0700

    chore(util): add function to get req.storeContext

--- backend/src/common/utils/index.ts
+++ backend/src/common/utils/index.ts
@@ -1,3 +1,3 @@
 export { sendResponse } from './api-response.util.js';
 export { logger } from './logger.util.js';
-export { requireReqUser } from './require-req-user.js';
+export * from './require-req.js';
--- backend/src/modules/access-control/require-permission.middleware.ts
+++ backend/src/modules/access-control/require-permission.middleware.ts
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes';
 
 import { accessControlService } from './access-control.service.js';
 import { CustomError } from '../../common/errors/index.js';
+import { requireReqStoreContext } from '../../common/utils/index.js';
 
 import type { AppRole, AppPermission } from './app-role-permission.type.js';
 import type { Request, NextFunction, Response } from 'express';
@@ -9,16 +10,7 @@ import type { Request, NextFunction, Response } from 'express';
 export const requirePermission = (permission: AppPermission) => {
   return (req: Request, _res: Response, next: NextFunction): void => {
     try {
-      const storeContext = req.storeContext;
-
-      if (!storeContext) {
-        throw new CustomError({
-          message:
-            'Store context is missing. Please ensure requireStoreContext middleware is used.',
-          status: StatusCodes.INTERNAL_SERVER_ERROR,
-        });
-      }
-
+      const storeContext = requireReqStoreContext(req);
       const role = storeContext.role.toUpperCase() as AppRole;
 
       const isAllowed = accessControlService.hasPermission({
