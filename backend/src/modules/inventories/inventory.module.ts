cfaf2c65bd69569f122cd539f8bd4b5ab4d52632
HowardPeter <phuocnt1611@gmail.com>
2026-03-25 22:36:22 +0700

    fix(product-package): add following deleted inventory when delete a package

--- backend/src/modules/inventories/inventory.module.ts
+++ backend/src/modules/inventories/inventory.module.ts
@@ -4,11 +4,12 @@ Service và Controller với nhau.
 Các module hoặc route khác sẽ import
 controller/service từ file này để sử dụng. */
 
+import { prisma } from '../../db/prismaClient.js';
 import { InventoryController } from '../inventories/controller/inventory.controller.js';
 import { InventoryRepository } from '../inventories/repository/inventory.repository.js';
 import { InventoryService } from '../inventories/service/inventory.service.js';
 
-const inventoryRepository = new InventoryRepository();
+const inventoryRepository = new InventoryRepository(prisma);
 const inventoryService = new InventoryService(inventoryRepository);
 
 export const inventoryController = new InventoryController(inventoryService);
