fe87330d940a01030b9bd1fd79c8f0fd31c37904
HowardPeter <phuocnt1611@gmail.com>
2026-04-01 01:45:44 +0700

    chore(search): edit max value of limit of validator

--- backend/src/modules/search/search.validator.ts
+++ backend/src/modules/search/search.validator.ts
@@ -11,7 +11,7 @@ export const searchByKeywordQuerySchema = z.object({
     .min(1, 'Keyword is required')
     .max(100, 'Keyword must be at most 100 characters'),
   page: z.coerce.number().int().min(1).optional().default(1),
-  limit: z.coerce.number().int().min(1).max(50).optional().default(50),
+  limit: z.coerce.number().int().min(1).max(100).optional().default(50),
 });
 
 const searchByPrefixQuerySchema = z.object({
