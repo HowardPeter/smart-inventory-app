4b2a6806e97a2aba1caa458947d158a5dad7b4f9
Hoàng Nam <bapnhn1810@gmail.com>
2026-04-08 17:40:48 +0700

    refactor: delete folder and file unuse in auth module

--- backend/src/modules/auth/validators/register-email-pw.validator.ts
+++
@@ -1,9 +0,0 @@
-// import { z } from 'zod';
-
-// export const registerSchema = z.object({
-//   email: z.string().email(),
-//   password: z.string().min(6),
-//   fullName: z.string().min(2),
-// });
-
-// export type RegisterInput = z.infer<typeof registerSchema>;
